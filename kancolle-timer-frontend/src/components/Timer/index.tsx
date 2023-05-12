ca3957b805393c3abee2214bed81a0931c94d5d9
chocono2230 <61158776+chocono2230@users.noreply.github.com>
2023-05-12 18:06:54 +0900

    並び替え機能の一時停止 #41 #42
    並び替え機能の削除

--- kancolle-timer-frontend/src/components/Timer/index.tsx
+++ kancolle-timer-frontend/src/components/Timer/index.tsx
@@ -1,56 +1,22 @@
 import { Box, Container, List } from '@mui/material';
-import {
-  DndContext,
-  closestCenter,
-  KeyboardSensor,
-  PointerSensor,
-  useSensor,
-  useSensors,
-  DragEndEvent,
-} from '@dnd-kit/core';
-import { SortableContext, sortableKeyboardCoordinates, verticalListSortingStrategy } from '@dnd-kit/sortable';
 
 import EditTimer from './EditTimer';
 import ShowTimer from './ShowTimer';
 import useTimerIndex from './useIndexTimer';
 
 const Timer = () => {
-  const { timersArray, organizeAfterDelete, changeTimerOrder } = useTimerIndex();
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    })
-  );
-
-  const handleDragEnd = (event: DragEndEvent) => {
-    const { active, over } = event;
-
-    if (!over) {
-      return;
-    }
-
-    if (active.id !== over.id) {
-      const oldIndex = timersArray.findIndex((v) => v.id === active.id);
-      const newIndex = timersArray.findIndex((v) => v.id === over.id);
-      changeTimerOrder(oldIndex, newIndex);
-    }
-  };
+  const { timersArray, organizeAfterDelete } = useTimerIndex();
 
   return (
     <>
       <Container maxWidth='sm'>
         <Box sx={{ minWidth: '450px' }}>
-          <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
-            <SortableContext items={timersArray} strategy={verticalListSortingStrategy}>
-              <List>
-                {timersArray &&
-                  timersArray.map((t) => {
-                    return <ShowTimer key={t.id} timer={t} organizeAfterDelete={organizeAfterDelete} />;
-                  })}
-              </List>
-            </SortableContext>
-          </DndContext>
+          <List>
+            {timersArray &&
+              timersArray.map((t) => {
+                return <ShowTimer key={t.id} timer={t} organizeAfterDelete={organizeAfterDelete} />;
+              })}
+          </List>
           <Box sx={{ m: 2 }}>
             <EditTimer timerListSize={timersArray.length} />
           </Box>

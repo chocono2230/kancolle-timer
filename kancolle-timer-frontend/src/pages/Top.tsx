cbbd06711cbe08bf9abde44cd694c6752e9ec9d0
chocono2230 <61158776+chocono2230@users.noreply.github.com>
2023-05-21 18:40:46 +0900

    テスト用コンポーネントの削除

--- kancolle-timer-frontend/src/pages/Top.tsx
+++ kancolle-timer-frontend/src/pages/Top.tsx
@@ -1,16 +1,9 @@
-import { useState } from 'react';
-import { ToggleButton } from '@mui/material';
 import Timer from '../components/Timer';
 
 const Top = () => {
-  const [flg, setFlg] = useState(false);
   return (
     <>
-      <ToggleButton value='check' selected={flg} onChange={() => setFlg(!flg)}>
-        {flg ? 'ON' : 'OFF'}
-      </ToggleButton>
-      <br />
-      {!flg && <Timer />}
+      <Timer />
     </>
   );
 };

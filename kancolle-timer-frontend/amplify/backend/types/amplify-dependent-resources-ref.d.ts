97cff9a569c3586c72b4c2bfb3e136cff93813ef
chocono2230 <61158776+chocono2230@users.noreply.github.com>
2023-08-06 20:21:12 +0900

    環境分離（フロントエンド） #57 (#59)
    * amplify CLI から環境追加
    
    * ddbの内容をコピーするスクリプト作成

--- kancolle-timer-frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ kancolle-timer-frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,9 @@
 export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "kancolletimerfronten": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
+  "api": {
+    "kancolletimerfronten": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
     }
+  }
 }
